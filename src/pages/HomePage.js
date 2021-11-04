36b643d57854c4bcfee99e17d89717b8372af27b
TanLoc2012 <loc.nguyencse@gmail.com>
2021-11-04 17:37:33 +0700

    HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Header from '../components/header/Header'
 import Footer from '../components/footer/Footer'
 import Carousel from '../components/Slider/Carousel'
+import Pizza from '../components/HotSale/components/Pizza'
+import Sushi from '../components/HotSale/components/Sushi'
+import TrangMieng from '../components/HotSale/components/TrangMieng';
 
 function HomePage() {
     
@@ -9,6 +12,9 @@ function HomePage() {
         <div style={{position: 'relative'}}>
             <Header></Header>
             <Carousel></Carousel>
+            <Pizza></Pizza> 
+            <Sushi></Sushi>
+            <TrangMieng></TrangMieng>
             <Footer></Footer>
         </div>
     );
