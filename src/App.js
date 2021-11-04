74b9c96facf2288b57eaf3c62b377d1d6f12ec84
TanLoc2012 <loc.nguyencse@gmail.com>
2021-11-04 21:01:33 +0700

    LoginPage,SignupPage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
@@ -11,6 +12,14 @@ function App() {
         
         <Route path="/" exact>
           <HomePage></HomePage>
+        </Route>
+
+        <Route path="/login">
+          <LoginPage></LoginPage>
+        </Route>
+
+        <Route path="/register">
+          <SignupPage></SignupPage>
         </Route>
 
       </Router>
